81025395efa884fb307121e69328cc1cd0e70395
hhaggan <hhaggan@hotmail.com>
2019-05-27 11:22:33 -0400

    Classifier with 95.6% confidence

--- Traffic-Sign-Classifier.py
+++ Traffic-Sign-Classifier.py
@@ -4,13 +4,30 @@ import math
 import os
 import random 
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import time
+from multiprocessing import Queue
+import pickle
 #tensorflow
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.contrib.layers import flatten
 
-#downloading data
+#loading data
+training_file = "traffic-signs-data/train.p"
+testing_file = "traffic-signs-data/test.p"
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+    
+X_train, y_train = train['features'], train['labels']
+X_test, y_test = test['features'], test['labels']
+
+#dividing the training dataset into training and validation
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
 #defining some hyperparameters
 n_classes = 43
@@ -19,11 +36,12 @@ batch_size = 128
 rate = 0.001
 
 #defining the features and labels
-x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
 y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, 43)
 
 #preprocess the data
+X_train, y_train = shuffle(X_train, y_train)
 
 #identifyign the Convolution Neural Network layers
 #following the same architecture as the LeNet but with different output
@@ -34,7 +52,7 @@ def traffic_classifier(x):
 
     #First convolution
     '''The Input for the images should be 28*28*6 for the first convolution layer'''
-    conv1_w = tf.Variable(tf.truncated_normal(shape=(5,5,1,6), mean = mu, stddev = sigma))
+    conv1_w = tf.Variable(tf.truncated_normal(shape=(5,5,3,6), mean = mu, stddev = sigma))
     conv1_b = tf.Variable(tf.zeros(6))
     conv1 = tf.nn.conv2d(x, conv1_w, strides=[1,1,1,1], padding='VALID') + conv1_b
 
@@ -46,7 +64,7 @@ def traffic_classifier(x):
     #Second Convolution
     '''The Input for the images should be 10*10*16 for the first convolution layer'''
     conv2_w = tf.Variable(tf.truncated_normal(shape=(5,5,6,16), mean=mu, stddev=sigma))
-    conv2_b = tf.Variable(tf.zeros(6))
+    conv2_b = tf.Variable(tf.zeros(16))
     conv2 = tf.nn.conv2d(conv1, conv2_w, strides=[1,1,1,1], padding='VALID') + conv2_b
 
     #Activation Function
@@ -78,17 +96,59 @@ def traffic_classifier(x):
 
     #Third Fully Connected Layer
     '''The Input for the images should be 28*28*6 for the first convolution layer'''
-    fc3_w = tf.Variable(tf.truncated_normal(shape=(84,10), mean=mu, stddev=sigma))
-    fc3_b = tf.Variable(tf.zeros(10))
+    fc3_w = tf.Variable(tf.truncated_normal(shape=(84,43), mean=mu, stddev=sigma))
+    fc3_b = tf.Variable(tf.zeros(43))
     logits = tf.matmul(fc2, fc3_w) + fc3_b
-    '''The Final output should be 10 as per the MNIST data'''
+    '''The Final output should be 43 as per the traffic sign data data'''
     return logits
 
+#Deep Learning details
 logits = traffic_classifier(x)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
 loss_function = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate=rate)
 training = optimizer.minimize(loss_function)
 
+#parameters for the correction of the training 
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+#function to evaluate
 def evaluate(x_data, y_data):
-    
+    num_examples = len(x_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range (0, num_examples, batch_size):
+        batch_x, batch_y = x_data[offset:offset+batch_size], y_data[offset:offset+batch_size]
+        accuracy = sess.run(accuracy_operation, feed_dict = {x:batch_x, y:batch_y})
+        total_accuracy += (accuracy*len(batch_x))
+    return total_accuracy/num_examples
+
+#Train The Model
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    print("Training...")
+    print()
+    for i in range(epochs):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, batch_size):
+            end = offset + batch_size
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training, feed_dict={x: batch_x, y: batch_y})
+            
+        validation_accuracy = evaluate(X_val, y_val)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+        
+    saver.save(sess, './lenet')
+    print("Model saved")
+
+'''with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+
+    test_accuracy = evaluate(X_test, y_test)
+    print("Test Accuracy = {:.3f}".format(test_accuracy))'''
